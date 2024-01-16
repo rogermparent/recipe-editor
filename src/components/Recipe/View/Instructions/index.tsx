054fe166b50313a1a901bbbcd476868e9457c687
rogermparent <rogermparent@gmail.com>
2024-01-16 13:07:04 -0500

    Add a style for instruction group child headings

--- src/components/Recipe/View/Instructions/index.tsx
+++ src/components/Recipe/View/Instructions/index.tsx
@@ -4,7 +4,8 @@ import { InstructionEntry } from "@/app/lib/models/recipes/types";
 
 import Markdown from "@/components/Markdown";
 
-const headingStyle = "text-lg font-bold my-2 border-b border-white";
+const stepHeadingStyle = "text-lg font-bold my-2 border-b border-white";
+const childHeadingStyle = "text-base font-bold my-1 border-b border-white";
 
 export const InstructionEntryView = ({
   entry,
@@ -15,11 +16,11 @@ export const InstructionEntryView = ({
     const { name, instructions } = entry;
     return (
       <li className="my-3 list-none">
-        {name && <h3 className={headingStyle}>{name}</h3>}
+        {name && <h3 className={stepHeadingStyle}>{name}</h3>}
         <ol className="list-decimal pl-1 sm:pl-3 md:pl-4">
           {instructions.map(({ name, text }, i) => (
             <li key={i} className="my-2">
-              {name && <h4>{name}</h4>}
+              {name && <h4 className={childHeadingStyle}>{name}</h4>}
               <Markdown>{text}</Markdown>
             </li>
           ))}
@@ -30,7 +31,7 @@ export const InstructionEntryView = ({
     const { name, text } = entry;
     return (
       <li className="my-3">
-        {name && <h3 className={headingStyle}>{name}</h3>}
+        {name && <h3 className={stepHeadingStyle}>{name}</h3>}
         <Markdown>{text}</Markdown>
       </li>
     );
