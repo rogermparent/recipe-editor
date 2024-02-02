fd7e45d6855bad9a86f29b88818bb2e310b30a11
rogermparent <rogermparent@gmail.com>
2024-02-02 13:21:34 -0500

    Format ingredient quantities on import

--- src/collections/recipes/util/parseIngredients.ts
+++ src/collections/recipes/util/parseIngredients.ts
@@ -1,11 +1,13 @@
 import { parseIngredient } from "parse-ingredient";
 import { Ingredient } from "@/collections/recipes/controller/types";
+import { formatQuantity } from "format-quantity";
 
 export function createIngredients(input: string) {
   return parseIngredient(input).map(
     ({ quantity, unitOfMeasure, description }) => {
+      const formattedQuantity = quantity ? formatQuantity(quantity) : undefined;
       const massagedIngredient: Ingredient = {
-        quantity: quantity ? String(quantity) : undefined,
+        quantity: formattedQuantity || undefined,
         unit: unitOfMeasure || undefined,
         ingredient: description,
       };
