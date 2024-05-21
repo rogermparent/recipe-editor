c06ea7ee221dae8a4ae008c9a0278ebb64400338
Roger <rogermparent@gmail.com>
2024-05-21 19:00:31 -0400

    Add basic print styles to recipe editor page (#26)

--- packages/recipes-collection/components/View/index.tsx
+++ packages/recipes-collection/components/View/index.tsx
@@ -34,7 +34,7 @@ export const RecipeView = ({
   return (
     <MultiplierProvider>
       <div className="w-full h-full p-2 print:p-0 grow flex flex-col flex-nowrap">
-        <div className="container mx-auto lg:flex lg:flex-row justify-center">
+        <div className="container mx-auto lg:flex lg:flex-row justify-center print:w-full print:max-w-full">
           {image && (
             <RecipeImage
               slug={slug}
@@ -43,11 +43,11 @@ export const RecipeView = ({
               width={580}
               height={450}
               sizes="100vw"
-              className="object-cover aspect-ratio-[16/10] w-full h-96 lg:max-w-96"
+              className="object-cover aspect-ratio-[16/10] w-full h-96 lg:max-w-96 lg:mr-4 print:hidden"
               loading="eager"
             />
           )}
-          <div className="lg:pl-4 flex-1 max-w-prose mx-auto">
+          <div className="flex-1 max-w-prose mx-auto lg:mx-0 print:max-w-full">
             <h1 className="text-3xl font-bold mt-4 mb-6">{name}</h1>
             {description && (
               <div className="my-2">
@@ -65,10 +65,10 @@ export const RecipeView = ({
             </div>
           </div>
         </div>
-        <div className="lg:flex lg:flex-row lg:px-2 justify-center flex-nowrap container mx-auto">
+        <div className="justify-center flex-nowrap container mx-auto p-2 lg:flex lg:flex-row print:w-full print:max-w-full print:flex print:flex-row">
           <Ingredients ingredients={ingredients} />
           {instructions && (
-            <div className="max-w-prose mx-auto">
+            <div className="max-w-prose mx-auto lg:mx-0 print:w-full print:max-w-full">
               <h2 className="text-xl font-bold my-3">Instructions</h2>
               <ol className="list-decimal pl-4">
                 {instructions.map((entry, i) => (
