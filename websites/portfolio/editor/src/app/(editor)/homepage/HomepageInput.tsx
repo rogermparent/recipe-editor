c0b9f0ffffbe8ae37354cac224cd69cf63bf315a
rogermparent <rogermparent@gmail.com>
2024-05-22 19:53:00 -0400

    Add highlight control B

--- websites/portfolio/editor/src/app/(editor)/homepage/HomepageInput.tsx
+++ websites/portfolio/editor/src/app/(editor)/homepage/HomepageInput.tsx
@@ -1,9 +1,64 @@
 "use client";
 
 import {
+  DefaultControls,
+  FormatButton,
+  MarkdownControlsProps,
   MarkdownInput,
   MarkdownInputProps,
+  wrapSelection,
 } from "component-library/components/Form/inputs/Markdown";
+import { MouseEventHandler } from "react";
+
+export function HighlightControl({
+  textAreaRef,
+  setValue,
+  setSelectionRange,
+  onChange,
+}: MarkdownControlsProps) {
+  const textArea = textAreaRef.current;
+
+  const handleHighlightClick: MouseEventHandler<HTMLButtonElement> = () => {
+    wrapSelection({
+      prefix: "<mark>",
+      suffix: "</mark>",
+      textArea,
+      setValue,
+      setSelectionRange,
+      onChange,
+    });
+  };
+
+  return (
+    <FormatButton onClick={handleHighlightClick}>
+      <mark className="bg-yellow-200">H</mark>
+    </FormatButton>
+  );
+}
+
+export function CustomControls({
+  textAreaRef,
+  setValue,
+  setSelectionRange,
+  onChange,
+}: MarkdownControlsProps) {
+  return (
+    <>
+      <DefaultControls
+        textAreaRef={textAreaRef}
+        setValue={setValue}
+        setSelectionRange={setSelectionRange}
+        onChange={onChange}
+      />
+      <HighlightControl
+        textAreaRef={textAreaRef}
+        setValue={setValue}
+        setSelectionRange={setSelectionRange}
+        onChange={onChange}
+      />
+    </>
+  );
+}
 
 export default function HomepageMarkdownInput({
   name,
@@ -21,6 +76,7 @@ export default function HomepageMarkdownInput({
       defaultValue={defaultValue}
       onChange={onChange}
       errors={errors}
+      Controls={CustomControls}
     />
   );
 }
